7819cdd7c83be4be19bf63fdddd10bbd542b57a2
Ayshi <ayshibd@gmail.com>
2023-05-17 12:50:21 +0600

    change url

--- src/components/url.js
+++ src/components/url.js
@@ -1,3 +1,4 @@
 // const url = 'http://localhost:5000'
-const url = 'https://cakencreamy.onrender.com'
+// const url = 'https://cakencreamy.onrender.com'
+const url ='https://as-83s5.onrender.com'
 export default url ;
